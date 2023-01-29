a9a3d3532022e32b426d8e6dfdadecf461368c53
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-01-29 08:44:34 -0600

    Clase base

---
+++ PostfixCalculator.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class PostfixCalculator implements IPostfixCalculator {
+
+    @Override
+    public boolean isOneItem(IStack operandos) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int suma(int a, int b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int resta(int a, int b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int multiplicacion(int a, int b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int division(int a, int b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isOperator(String item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ArrayList<String> getItems(String _expresion) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
