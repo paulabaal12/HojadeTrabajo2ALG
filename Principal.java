6bd9cf4a635b56fd2232258b0d0cb77a207b1728
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-01-30 17:00:17 -0600

    Principal

---
+++ Principal.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader;
+        String line;
+        int noOfLines=0;
+		reader = new BufferedReader(new FileReader("Text.txt"));
+        StackHandmade<Integer> calc = new StackHandmade<Integer>();
+        PostfixCalculator postCalc= new PostfixCalculator();
+        while (reader.readLine() != null) {
+            noOfLines++;
+        }
+        reader = new BufferedReader(new FileReader("Text.txt"));
+        for(int l=0; l<noOfLines;l++){
+            line = reader.readLine();
+            for(int i=0; i<line.length(); i++){
+                char c = line.charAt(i);
+                int a =0;
+                int b=0;
+                if(Character.isDigit(c)){
+                    int t = Character.getNumericValue(c);
+                        calc.push(t);
+                    }
+                    else if(c == '+'){
+                        a = calc.pull();
+                        b = calc.pull();
+                        calc.push(postCalc.suma(a, b));
+                    }
+                    else if(c == '-'){
+                    a = calc.pull();
+                    b = calc.pull();
+                    calc.push(postCalc.resta(a, b));
+                    }
+                    else if(c == '*'){
+                    a = calc.pull();
+                    b = calc.pull();
+                    calc.push(postCalc.multiplicacion(a, b));
+                    }
+                    else if(c == '/'){
+                    a = calc.pull();
+                    b = calc.pull();
+                    calc.push(postCalc.division(a, b));
+                    }
+            }
+            System.out.print(line+" = "+calc.pull()+"\n");
+        } 
+        
+
+    }
+        
+     
+    
+}
